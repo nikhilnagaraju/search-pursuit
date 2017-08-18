996d9d3f74ab1318d2838bce8b8f625a15780b87
nikhilnagaraju96 <nikhilnagaraju96@gmail.com>
2017-08-19 02:50:22 +0530

    Basic setup

---
+++ spec/search-pursuit-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SearchPursuitView from '../lib/search-pursuit-view';
+
+describe('SearchPursuitView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
